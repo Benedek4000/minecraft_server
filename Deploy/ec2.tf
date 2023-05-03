7b1657b95f91ef28c2230a4e7bb21394363d3130
Benedek4000 <kovacsbenedek4000@gmail.com>
2023-05-03 12:22:45 +0100

    stopServer now returns server is stopped instead of failure when server is stopped

--- Deploy/ec2.tf
+++ Deploy/ec2.tf
@@ -42,7 +42,6 @@ data "aws_ami" "ubuntu" {
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-${data.aws_ec2_instance_type.server.supported_architectures[0]}-server-*"]
-    #values = ["al2023-ami-*-kernel-6.1-${data.aws_ec2_instance_type.server.supported_architectures[0]}"]
   }
   filter {
     name   = "root-device-type"
