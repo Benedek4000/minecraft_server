29c5539fb889c910b0e5300619c7a34887225442
Benedek4000 <kovacsbenedek4000@gmail.com>
2023-05-24 11:13:02 +0100

    added bucket acl

--- Deploy/modules/s3_bucket/main.tf
+++ Deploy/modules/s3_bucket/main.tf
@@ -49,6 +49,20 @@ resource "aws_s3_bucket" "bucket" {
   bucket_prefix = var.bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+
+  depends_on = [aws_s3_bucket_ownership_controls.bucket]
+}
+
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket                  = aws_s3_bucket.bucket.id
   block_public_acls       = true
