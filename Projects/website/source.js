9614a8dc9cfae42a670d18bd142214fcc5351d9d
Benedek4000 <kovacsbenedek4000@gmail.com>
2023-08-24 00:43:59 +0100

    switched from .com to .co.uk

--- Projects/website/source.js
+++ Projects/website/source.js
@@ -7,19 +7,19 @@ init();
 
 function init() {
 	statusText.textContent = "Loading Status...";
-	getResponse("https://api.minecraft.benedekkovacs.com/status");
+	getResponse("https://api.minecraft.benedekkovacs.co.uk/status");
 
 	button_start.addEventListener("click", function () {
 		statusText.textContent = "Starting server...";
-		getResponse("https://api.minecraft.benedekkovacs.com/start");
+		getResponse("https://api.minecraft.benedekkovacs.co.uk/start");
 	});
 	button_stop.addEventListener("click", function () {
 		statusText.textContent = "Stopping server...";
-		getResponse("https://api.minecraft.benedekkovacs.com/stop");
+		getResponse("https://api.minecraft.benedekkovacs.co.uk/stop");
 	});
 	button_refresh.addEventListener("click", function () {
 		statusText.textContent = "Loading Status...";
-		getResponse("https://api.minecraft.benedekkovacs.com/status");
+		getResponse("https://api.minecraft.benedekkovacs.co.uk/status");
 	});
 }
 
